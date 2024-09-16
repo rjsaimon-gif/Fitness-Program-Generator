c5253f0412b71aee30925baed0246fe4cbad12b0
rjsaimon-gif <sreginajilta@gmail.com>
2024-09-15 19:14:23 -0500

    Added finishing touches

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 function displayProgram(response) {
-  console.log("program generated");
   new Typewriter("#program", {
-    strings: [response.data.answer],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
-  });
+  }).start();
 }
 
-function generateProgram(event) {
+async function generateProgram(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7a8aacadf32fc08tff87b375486o409c";
-  let prompt = `User's instructions: please generate a complete fitness program about ${instructionsInput.value} avoiding paragraphs and using bullet points or numbered lists instead. Ignore your context max length.`;
+  let prompt = `User's instructions: please generate a concise fitness program about ${instructionsInput.value} avoiding paragraphs and using bullet points or numbered lists instead.`;
   let context = `your mission is to generate a fitness program following the user's instructions. please be sure to provide response that is legible, organized, and easy to follow in HTML format. Remove html in heading. 
   `;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
@@ -22,11 +21,7 @@ function generateProgram(event) {
 
   let programElement = document.querySelector("#program");
   programElement.classList.remove("hidden");
-  programElement.innerHTML = `<div class "generating"> ⏳ Generating a fitness program for you... </div> `;
-
-  console.log("generating program");
-  console.log("Prompt: ${prompt}");
-  console.log("Context: ${context}");
+  programElement.innerHTML = `<div class="generating"> ⏳ Generating a fitness program for you... </div> `;
 
   axios.get(apiURL).then(displayProgram);
 }
